f54efcd2b99369a1ca0bfcee8b94b2a8d2a39b65
JayAree999 <jayyqwe123@gmail.com>
2023-03-02 20:26:37 +0700

    Add select playlists not sure if fetching works

--- my-app/src/Select.js
+++ my-app/src/Select.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { getPlaylistTracks } from './spotify';
 
 const Select = () => {
-    const [numberOfQuestions, setNumberOfQuestions] = useState(10);
-    const [timeLimit, setTimeLimit] = useState(30);
     const location = useLocation();
-    const forceLogin = location.state && location.state.forceLogin;
-
-    useEffect(() => {
-        if (forceLogin) {
-            // TODO: fetch playlists and set selected playlist id
-        }
-    }, [forceLogin]);
+    const { state } = location;
+    const [numberOfQuestions, setNumberOfQuestions] = useState('10');
+    const [timeLimit, setTimeLimit] = useState('60');
+    const [playlists, setPlaylists] = useState([]);
+    const [selectedPlaylistId, setSelectedPlaylistId] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleNumberOfQuestionsChange = (event) => {
         setNumberOfQuestions(event.target.value);
@@ -21,26 +19,91 @@ const Select = () => {
         setTimeLimit(event.target.value);
     };
 
+    const handlePlaylistChange = (event) => {
+        setSelectedPlaylistId(event.target.value);
+    };
+
+    useEffect(() => {
+        const fetchPlaylists = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('https://api.spotify.com/v1/me/playlists', {
+                    headers: {
+                        Authorization: `Bearer ${state.accessToken}`,
+                    },
+                });
+                const data = await response.json();
+                setPlaylists(data.items);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        if (state.accessToken) {
+            fetchPlaylists();
+        }
+    }, [state.accessToken]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await getPlaylistTracks(selectedPlaylistId, state.accessToken);
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
-            <h1>Select Quiz Options</h1>
-            <label htmlFor="number-of-questions">Number of questions:</label>
-            <select id="number-of-questions" value={numberOfQuestions} onChange={handleNumberOfQuestionsChange}>
-                <option value={5}>5</option>
-                <option value={10}>10</option>
-                <option value={15}>15</option>
-            </select>
-            {forceLogin && (
-                <>
-                    <br />
-                    <label htmlFor="time-limit">Time limit (seconds):</label>
-                    <select id="time-limit" value={timeLimit} onChange={handleTimeLimitChange}>
-                        <option value={30}>30</option>
-                        <option value={60}>60</option>
-                        <option value={90}>90</option>
+            <h1>Select your quiz settings</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Number of questions:
+                    <select value={numberOfQuestions} onChange={handleNumberOfQuestionsChange}>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="30">30</option>
                     </select>
-                </>
-            )}
+                </label>
+                <br />
+                <br />
+                <label>
+                    Time limit (in seconds):
+                    <select value={timeLimit} onChange={handleTimeLimitChange}>
+                        <option value="30">30</option>
+                        <option value="60">60</option>
+                        <option value="90">90</option>
+                    </select>
+                </label>
+                {state.forceLogin && (
+                    <>
+                        <br />
+                        <br />
+                        <label>
+                            Playlist:
+                            <select value={selectedPlaylistId || ''} onChange={handlePlaylistChange}>
+                                {isLoading ? (
+                                    <option value="">Loading playlists...</option>
+                                ) : (
+                                    <>
+                                        <option value="">Select a playlist</option>
+                                        {playlists.map((playlist) => (
+                                            <option key={playlist.id} value={playlist.id}>
+                                                {playlist.name}
+                                            </option>
+                                        ))}
+                                    </>
+                                )}
+                            </select>
+                        </label>
+                    </>
+                )}
+                <br />
+                <br />
+                <button type="submit">Start Quiz</button>
+            </form>
         </div>
     );
 };
