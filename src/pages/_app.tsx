af5562fe5c93826ee4ba6925cc50f32d47ce7b38
sunniez <ssunniez@gmail.com>
2023-03-22 13:38:52 +0700

    yay

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,9 @@
 import '@/styles/globals.css'
+import '../styles/App.css'
+import '../styles/index.css'
+import '../styles/QuizPage.css'
+import '../styles/AudioPlayer.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
